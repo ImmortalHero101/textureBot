5e0269b70caf452fbaae8e30bbaf574fa6656af9
ImmortalHero <31122950+ImmortalHero101@users.noreply.github.com>
2019-09-09 10:23:29 +0500

    Create message.js

---
+++ events/message.js
@@ -0,0 +1,11 @@
+module.exports = function(client, message) {
+  if (client.stopper || message.author.bot || message.guild && message.guild.id !== "415019865800310793" && message.channel.id !== "618568997494849536") return;
+  let [commandName, ...splitArgs] = message.content.slice(1).split(" ");
+  try {
+    let selectedCommand = client.commands.get(commandName);
+    if (!selectedCommand) return;
+    selectedCommand(client, message, splitArgs);
+  } catch (e) {
+    return message.channel.send("An error occured while running the command. Please contact the mods about this issue.");
+  }
+}
