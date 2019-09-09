91a28d44794fef95fee4290156d855495998d448
ImmortalHero <31122950+ImmortalHero101@users.noreply.github.com>
2019-09-09 22:36:12 +0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,8 +1,11 @@
 module.exports = function(client, message) {
-  if (client.stopper || message.author.bot || message.guild && (message.guild.id !== "415019865800310793" && message.channel.id !== "618568997494849536") || message.guild.id !== "620583116586876970") return;
+  console.log("Condition:", client.stopper || message.author.bot || message.guild && ((message.guild.id !== "415019865800310793" && message.channel.id !== "618568997494849536") || message.guild.id !== "620583116586876970"));
+  if (client.stopper || message.author.bot || message.guild && ((message.guild.id !== "415019865800310793" && message.channel.id !== "618568997494849536") || message.guild.id !== "620583116586876970")) return;
   let [commandName, ...splitArgs] = message.content.slice(1).split(" ");
+  console.log(commandName);
   try {
     let selectedCommand = client.commands.get(commandName);
+    console.log(selectedCommand)
     if (!selectedCommand) return;
     selectedCommand(client, message, splitArgs.join(" "));
   } catch (e) {
