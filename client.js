3d2cd75c6d5cb00fcd3d5c0d12b137dd55ad6cca
ImmortalHero <31122950+ImmortalHero101@users.noreply.github.com>
2019-09-09 11:20:11 +0500

    Update client.js

--- client.js
+++ client.js
@@ -1,11 +1,25 @@
 const fs = require("fs"),
       Discord = require("discord.js"),
+      mongoose = require("mongoose"),
       client = new Discord.Client(),
+      Schema = mongoose.Schema,
       TOKEN = process.env.TOKEN,
       port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
       ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 client.commands = new Discord.Collection();
+
+let SubmissionSchema = new Schema({
+  instanceType: String, // Entity, Block, Item
+  suggestions: [String, String] // User ID, Suggestion
+  instanceURL: {type: String, default: ""}
+  creation: {type: Integer, default: Date.now()},
+  lastUpdate: {type: Integer, default: 0},
+  updates: {type: Integer, default: 0},
+  author: String // Author ID
+});
+
+client.dataModel = mongoose.model("dataModel", SubmissionSchema);
 
 fs.readdir("./events/", (err, eventFiles) => {
   if (err) return console.error(err);
